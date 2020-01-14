aa6edbbc73cdead74b184c5243e4258bfec197dd
Carrie <carrie.g.crow@gmail.com>
2020-01-14 22:23:55 +0000

    Added tests and refactored check left and right methods

--- lib/binary_tree_sort.rb
+++ lib/binary_tree_sort.rb
@@ -7,22 +7,6 @@ class BinaryTree
     @right = right
   end
 
-  def self.check_left(value, node)
-    if node.left.nil?
-      node.left = BinaryTree.new(value, nil, nil)
-    else
-      compare_value(value, node.left)
-    end
-  end
-
-  def self.check_right(value, node)
-    if node.right.nil?
-      node.right = BinaryTree.new(value, nil, nil)
-    else
-      compare_value(value, node.right)
-    end
-  end
-
   def self.compare_value(value, node)
     if value < node.payload
       check_left(value, node)
@@ -45,14 +29,18 @@ class BinaryTree
 
     @sorted_array = []
 
-    puts @trunk.inspect
-    #Create first node, because the tree is currently nil
-
-    #Create second node, to the left if it is less or the right if it is more
-
     #After all items are placed, extract the lowest child
 
     #circle up to all of the other children
 
+  end
+  
+# private
+  def check_left
+    self.left.nil? ? nil : self.left.payload
+  end
+
+  def check_right
+    self.right.nil? ? nil : self.right.payload
   end
 end
--- spec/binary_tree_sort_spec.rb
+++ spec/binary_tree_sort_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe :BinaryTree, type: :model do 
+  before :each do
+      #Test tree creation
+      @seven = BinaryTree.new(7, nil, nil)
+      @five  = BinaryTree.new(5, nil, nil)
+      @four  = BinaryTree.new(4, nil, nil)
+      @six   = BinaryTree.new(6, nil, @seven)
+      @three = BinaryTree.new(3, nil, @six)
+      @two   = BinaryTree.new(2, @four, @five)
+      @trunk = BinaryTree.new(1, @two, @three)
+  end
   describe "array sorts each item into the binary tree" do
     it "should take an array and place them in the appropriate place on a tree" do
-      #Test tree creation
-      seven = BinaryTree.new(7, nil, nil)
-      five  = BinaryTree.new(5, nil, nil)
-      four  = BinaryTree.new(4, nil, nil)
-      six   = BinaryTree.new(6, nil, seven)
-      three = BinaryTree.new(3, nil, six)
-      two   = BinaryTree.new(2, four, five)
-      trunk = BinaryTree.new(1, two, three)
-
       #Test case to sort
-      expect(trunk.payload).to eq 1
-      expect(trunk.left.left.payload).to eq 4
+      expect(@trunk.payload).to eq 1
+      expect(@trunk.left.left.payload).to eq 4
     end
   end
   describe 'binary tree sort should work' do
@@ -22,6 +23,20 @@ RSpec.describe :BinaryTree, type: :model do
       expected = [1, 4, 6, 7, 9, 10, 14]
       array = [7, 4, 9, 1, 6, 14, 10]
       expect(BinaryTree.sort(array)).to eq expected
+    end
+  end
+  describe 'binary tree should be able to check its nodes' do
+    it 'should return the left value' do
+      expect(@trunk.check_left).to eq(2)
+    end
+    it 'should return the left value when nil' do
+      expect(@three.check_left).to eq(nil)
+    end
+    it 'should return the right value' do
+      expect(@trunk.check_right).to eq(3)
+    end
+    it 'should return the right value if nil' do
+      expect(@four.check_right).to eq(nil)
     end
   end
 end
